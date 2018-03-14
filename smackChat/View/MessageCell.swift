3f1a1cdce88c26a6d0e0098a457107c4c2abf3f2
Rehan Parkar <parkarcamp@gmail.com>
2018-03-13 23:24:22 -0500

    timestamp feature added

--- smackChat/View/MessageCell.swift
+++ smackChat/View/MessageCell.swift
@@ -28,6 +28,20 @@ class MessageCell: UITableViewCell {
         userNameLabel.text = message.userName
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        let newFOrmatter = DateFormatter()
+        newFOrmatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFOrmatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
     }
 
 }
